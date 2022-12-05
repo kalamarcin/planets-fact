1a94f015a011411873d0a21eb786af00c820df71
Marcin Kala <kalamarcin1@gmail.com>
2022-12-05 12:05:14 +0100

    navbar mobile css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import { Route, Routes } from 'react-router-dom'
-import Nav from './Components/Nav/HomeLink'
+//pages
+
 import Home from './Pages/Home/Home'
-// import './styles/App.scss'
+import Mercury from './Pages/Mercury/Mercury'
+import Venus from './Pages/Venus/Venus'
+import Earth from './Pages/Earth/Earth'
+import Mars from './Pages/Mars/Mars'
+import Jupiter from './Pages/Jupiter/Jupiter'
+import Saturn from './Pages/Saturn/Saturn'
+import Uranus from './Pages/Uranus/Uranus'
+import Neptune from './Pages/Neptune/Neptune'
+//styles
+import './styles/App.scss'
+import './styles/Header.scss'
+import Navbar from './Components/navbar/Navbar'
+
 
 const App = () => {
   return (
     
-    <div className="App">
-      <Nav />
 
+    <div className="app">
+      <div className="header">
+        <h1>THE PLANETS</h1>
+        <Navbar />
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/mercury" element={<Mercury />} />
+        <Route path="/venus" element={<Venus />} />
+        <Route path="/earth" element={<Earth />} />
+        <Route path="/mars" element={<Mars />} />
+        <Route path="/jupiter" element={<Jupiter />} />
+        <Route path="/saturn" element={<Saturn />} />
+        <Route path="/uranus" element={<Uranus />} />
+        <Route path="/neptune" element={<Neptune />} />
 
       </Routes>
     </div>
