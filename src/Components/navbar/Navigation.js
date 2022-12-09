887f875149e1c970702a73e71cb824592772c187
Marcin Kala <kalamarcin1@gmail.com>
2022-12-09 15:30:01 +0100

    NavLink color

--- src/Components/navbar/Navigation.js
+++ src/Components/navbar/Navigation.js
@@ -1,15 +1,18 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { data } from '../../data'
 import '../../styles/Header.scss'
 
-const Navigation = () => {
+const Navigation = ({isActive}) => {
   return (
+    
     <div className="navigation-full">
       <ul>
         {data.map((item, index) => {
           return (
+            
             <li key={index}>
-              <Link to={`/${item.name.toLowerCase()}`}>{item.name.toUpperCase()}</Link>
+              
+              <NavLink className={item.name.toLowerCase()} to={`/${item.name.toLowerCase()}`}>{item.name.toUpperCase()}</NavLink>
             </li>
           )
         })}
